cab829671fe60e6ab910c36748c795494b878562
Luís Sérgio <luissergioaraujo@yahoo.com.br>
2025-11-04 14:47:51 -0300

    Exclude console.log from index.js

--- 02-the-module-system/01-revealing-module-pattern/index.js
+++ 02-the-module-system/01-revealing-module-pattern/index.js
@@ -1,15 +1,15 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "private" }] */
 
 const myModule = (() => {
-    const privateFoo = () => {}
-    const privateBar = []
-    const exported = {
-      publicFoo: () => {},
-      publicBar: () => {}
-    }
-    return exported
+  const privateFoo = () => {}
+  const privateBar = []
+  const exported = {
+    publicFoo: () => {},
+    publicBar: () => {}
   }
+  return exported
+}
 )() // once the parenthesis here are parsed, the function will be invoked
 
 console.log(myModule)
-console.log(myModule.privateFoo, myModule.privateBar)
+// console.log(myModule.privateFoo, myModule.privateBar)
