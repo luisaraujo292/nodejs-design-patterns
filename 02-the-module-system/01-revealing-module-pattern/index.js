da475563c86065bac6e870e068c5e88f6056a10b
Luís Sérgio <luissergioaraujo@yahoo.com.br>
2025-11-04 16:04:49 -0300

    Tests

--- 02-the-module-system/01-revealing-module-pattern/index.js
+++ 02-the-module-system/01-revealing-module-pattern/index.js
@@ -1,18 +1,1 @@
-/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "private" }] */
-
-/*
-const myModule = (() => {
-  const privateFoo = () => {}
-  const privateBar = []
-  const exported = {
-    publicFoo: () => {},
-    publicBar: () => {}
-  }
-  return exported
-}
-)()
-*/
-// once the parenthesis here are parsed, the function will be invoked
-
-console.log('myModule')
-// console.log(myModule.privateFoo, myModule.privateBar)
+console.log('Boa tarde!')
